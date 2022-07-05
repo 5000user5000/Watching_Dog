6bc6bbd706bf6e2fb621fa49f49c356ce26c1989
5000user5000 <37620231+5000user5000@users.noreply.github.com>
2022-07-05 17:15:53 +0800

    Update WatchingDogService.cs

--- WatchingDogService.cs
+++ WatchingDogService.cs
@@ -30,6 +30,9 @@ namespace Watching_Dog
             fileSystemWatcher.Changed += DirectoryChanged;
             fileSystemWatcher.Renamed += DirectoryChanged;
 
+            var serviceLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location); // c:\\Program Files\\Folder To Watch\\Watching_Dog.exe (因為其他人的檔案處會不同,用此得到現在位置)
+            File.AppendAllText($"{serviceLocation}\\log.txt", "service started!\n"); //本來沒有這個log.txt,這裡生成,並把msg的東西寫入
+
         }
 
         private void DirectoryChanged(object sender, FileSystemEventArgs e)
@@ -38,25 +41,33 @@ namespace Watching_Dog
             var serviceLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location); // c:\\Program Files\\Folder To Watch\\Watching_Dog.exe (因為其他人的檔案處會不同,用此得到現在位置)
             File.AppendAllText($"{serviceLocation}\\log.txt", msg); //本來沒有這個log.txt,這裡生成,並把msg的東西寫入
 
-            // char lastAddr = e.FullPath[-1];//取最後一個字元,會出錯
-            //int lastIndex = e.FullPath.Length - 1;
-
+            //char lastAddr = e.FullPath[-1];//取最後一個字元,會出錯
+          
 
             //Directory.CreateDirectory(@"D:\ForWatchingDog\changed");
             // File.AppendAllText($"{serviceLocation}\\log.txt", "dir created!\n");
             if (!System.IO.Directory.Exists(@"D:\ForWatchingDog\changed"))//如果不存在此檔案就創一個
             {
                 Directory.CreateDirectory(@"D:\ForWatchingDog\changed");
+                File.AppendAllText($"{serviceLocation}\\log.txt", "dir created!\n");
             }
-            /* if (lastAddr == '\\')
-             {
-                 System.IO.File.Move(e.FullPath, @"D:\ForWatchingDog\changed\");//移動檔案
 
-             }
-             else
-             {
-                 System.IO.Directory.Move(e.FullPath, @"D:\ForWatchingDog\changed\");//移動目錄到指定地點
-             }*/
+            string filename = Path.GetFileName(e.FullPath);
+            string fileLocation = @"D:\ForWatchingDog\changed\" + filename;//file要移動到的新地點
+           // File.Copy(e.FullPath, fileLocation);//複製檔案 System.IO的功能
+           string changeType =$"{e.ChangeType}";//這樣轉成string,不能直接e.ChangeType
+            if (Directory.Exists(e.FullPath) && changeType == "Renamed")
+            {
+                Directory.Move(e.FullPath, fileLocation);//移動目錄到指定地點
+               
+
+            }
+            else if(File.Exists(e.FullPath))
+            {
+
+                 File.Move(e.FullPath, fileLocation);//移動檔案 System.IO的功能
+                //File.Copy(e.FullPath, fileLocation);//複製檔案 System.IO的功能
+            }
 
 
 
